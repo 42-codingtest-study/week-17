e1432d3281b0c2faca8b67d6665b3d50791a4017
Chanwoong1 <jcw7468@gmail.com>
2023-03-15 12:24:35 +0900

    feat: 3/15

---
+++ 5523.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+a, b = 0, 0
+for i in range(int(input())) :
+    aa, bb = map(int, input().split())
+    if aa > bb : a += 1
+    elif aa < bb : b += 1
+print(a, b)
